64f786e7c587596909bc3f9a20d62e0c0a5b58e7
Asd <sobir0620@gmail.com>
2026-01-30 12:09:21 +0500

    fast api qo'sildi va test qilindi

---
+++ app/app.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "Hello from the server"}
+
+@app.get("/ping")
+def ping():
+    return {"status": "ok"}
