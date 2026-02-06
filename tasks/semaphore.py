994d1bdaf994f3ddd3055586f61361b82bdc9e90
Sobir-stan <musorm123@gmail.com>
2026-02-06 14:25:37 +0500

    semaphore uyga vazifa bajarildi

--- tasks/semaphore.py
+++ tasks/semaphore.py
@@ -1,1 +1,19 @@
-.
+from asyncio import gather, sleep, run
+import asyncio
+
+semphore = asyncio.Semaphore(3)
+
+async def work(worker):
+    async with semphore:
+        print(f"{worker} ishni boshladim")
+        await sleep(2)
+        print(f"{worker} ishni tugatdim")
+
+async def main():
+    tasks = []
+    for i in range(7):
+        tasks.append(work(i+1))
+    await gather(*tasks)
+
+run(main())
+
