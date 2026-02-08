f85560c659c017937dd52087285f82a66f6ca2ef
muminov313 <muminovsh313@gmail.com>
2026-02-08 23:43:09 +0500

    task4

--- app/app.py
+++ app/app.py
@@ -1,21 +1,13 @@
-from fastapi import FastAPI, Request, HTTPException, Form
+from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from pathlib import Path
-from app.schemas import Body_test
+from pydantic import BaseModel, Field, StrictStr, EmailStr
+
 
 app = FastAPI()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# @app.get("/", response_class=HTMLResponse)
-# def home():
-#     with open(INDEX_FILE, "r", encoding="utf-8") as f:
-#         return f.read()
-#
-# class userLoginShcemema(BaseModel):
-#     username: str
-#     age: int = Field(ge = 0, le=100)
 
 @app.get("/product/{id}")
 def login(id : int):
@@ -28,18 +20,20 @@ def login(param : int):
     print(param)
     return {"param": param}
 
-@app.post("/body_test")
-def body_test(data : Body_test):
-    print(data)
-    return data
-
 
 @app.get("/test", response_class=HTMLResponse)
 def test_display_html():
     with open(BASE_DIR/"frontend/login.html", "r", encoding="utf-8") as f:
-            return f.read()
+        return f.read()
+
+
+class UserSchema(BaseModel):
+    username: str = Field(min_length=3, max_length=50)
+    password: str = Field(min_length=1)
+    email: str
+
 
 @app.post("/send")
-def get_entered_data(text : dict):
-    print(text)
-    return {"text": text}
+def receive_user(user: UserSchema):
+    print(user)
+    return user
--- app/schemas.py
+++ app/schemas.py
@@ -1,6 +1,3 @@
 from pydantic import BaseModel, Field
 
 
-class Body_test(BaseModel):
-    username: str = Field(min_length=5, max_length=20)
-    age: int = Field(gt=0, le=100)
