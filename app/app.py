c850435c5539437c6eefa78bb68b1baa4eb52ba1
Sobir-stan <musorm123@gmail.com>
2026-02-18 16:02:45 +0500

    csv qoshildi + csv index error

--- app/app.py
+++ app/app.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI, Request, HTTPException, Form
 from fastapi.responses import HTMLResponse
 from pathlib import Path
-
 from starlette.staticfiles import StaticFiles
-
 from app.schemas import Body_test, New_user
 import pandas as pd
 
@@ -12,15 +10,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 app = FastAPI()
 app.mount("/static/scripts", StaticFiles(directory=BASE_DIR/"frontend/scripts"), name="static")
 
-
-# @app.get("/", response_class=HTMLResponse)
-# def home():
-#     with open(INDEX_FILE, "r", encoding="utf-8") as f:
-#         return f.read()
-#
-# class userLoginShcemema(BaseModel):
-#     username: str
-#     age: int = Field(ge = 0, le=100)
 
 @app.get("/product/{id}")
 def login(id : int):
@@ -43,14 +32,36 @@ def get_entered_data(text : dict):
     print(text)
     return {"text": text}
 
+def get_csv_path():
+    return BASE_DIR/"users.csv"
+
+def ensure_csv_exist():
+    csv_path = get_csv_path()
+
+    if not csv_path.exists():
+        df = pd.DataFrame(columns=("username", "email", "password"))
+        df.to_csv(csv_path, index=True)
+        return df
+    else:
+        return pd.read_csv(csv_path)
+
+def save_to_csv(user):
+    df = ensure_csv_exist()
+    csv_path = get_csv_path()
+
+    new_user_df = pd.DataFrame([user.model_dump()])
+    updated_df = pd.concat([df, new_user_df], ignore_index=True)
+    updated_df.to_csv(csv_path,index=False )
+
 @app.get("/register", response_class=HTMLResponse)
 def register_user():
     with open(BASE_DIR/"frontend/login.html", "r", encoding="utf-8") as f:
         return f.read()
 
-
 @app.post("/register")
 def register_user(user: New_user):
+    save_to_csv(user)
+
     print(user.username, user.email, user.password)
     return {"msg": "ok"}
 
