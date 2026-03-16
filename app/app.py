10c47727228855ca481a66785cf38558370f084e
Sobir-stan <musorm123@gmail.com>
2026-03-16 15:28:10 +0500

    sql progress tayyor

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request, HTTPException, Form
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import HTMLResponse
 from pathlib import Path
 from starlette.staticfiles import StaticFiles
@@ -23,6 +23,45 @@ def save_user(user):
     conn.commit()
     conn.close()
 
+def baseGetTableData(table, username):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute(f"SELECT * FROM {table} WHERE username = ?", (username,))
+    row = cursor.fetchone()
+    conn.close()
+    return row
+
+def getUser(username):
+    return baseGetTableData("users", username)
+
+def getProgress(username: str):
+    return baseGetTableData("progress", username)
+
+def create_progress(progress: SaveProgress):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute(
+        "INSERT INTO progress (username, cookies, totalCookies, cps, cursor_count, grandma_count, factory_count) VALUES (?, ?, ?, ?, ?, ?, ?)",
+        (progress.username, progress.cookies, progress.totalCookies, progress.cps, progress.cursor_count,
+         progress.grandma_count, progress.factory_count)
+    )
+    conn.commit()
+    conn.close()
+
+
+def update_progress(progress: SaveProgress):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute(
+        "UPDATE progress SET cookies = ?, totalCookies = ?, cps = ?, cursor_count = ?, grandma_count = ?, factory_count = ? WHERE username = ?",
+        (progress.cookies, progress.totalCookies, progress.cps, progress.cursor_count, progress.grandma_count,
+         progress.factory_count, progress.username)
+    )
+    conn.commit()
+    conn.close()
+
+
+
 
 @app.get("/register", response_class=HTMLResponse)
 def register_user():
@@ -41,15 +80,9 @@ def register_user():
     with open(BASE_DIR/"frontend/login.html", "r", encoding="utf-8") as f:
         return f.read()
 
-
 @app.post("/")
 def login(user: Login_user):
-
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM users WHERE username = ?", (user.username,))
-    row = cursor.fetchone()
-    conn.close()
+    row = getUser(user.username)
 
     user_db_password = str(row["password"])
 
@@ -67,40 +100,38 @@ def register_user():
 
 @app.post("/save_progress")
 def save_progress(progress : SaveProgress):
-    # # df = ensure_progress_csv_exist()
-    # # csv_path = get_progress_csv_path()
-    #
-    # if progress.username in df["username"].values:
-    #     indf = df[df["username"] == progress.username].index[0]
-    #
-    #     for col, val in progress.model_dump().items():
-    #         df.at[indf, col] = val
-    # else:
-    #     new_row_df = pd.DataFrame([progress.model_dump()])
-    #     df = pd.concat([df, new_row_df],ignore_index=True)
-    #     df.index.name = "id"
-    #
-    # df.to_csv(csv_path, index=True)
+
+    row = getProgress(progress.username)
+    if not row:
+        create_progress(progress)
+    else:
+        update_progress(progress)
+
     return {"msg": "progress saqlandi"}
 
 @app.get("/load_progress/{username}")
 def laod_progress(username: str):
-    # df = ensure_progress_csv_exist()
-    # user_row =df[df["username"] == username]
-    # if user_row.empty:
-    #
-    #     return {
-    #         "username": username,
-    #         "cookies" : 0.0,
-    #         "totalCookies" : 0.0,
-    #         "cps" : 0.0,
-    #         "cursor_count" : 0,
-    #         "grandma_count" : 0,
-    #         "factory_count" : 0,
-    #     }
-    #
-    # return user_row.iloc[0].to_dict()
-    pass
+    row = getProgress(username)
+    if not row:
+        return {
+                "username": username,
+                "cookies" : 0.0,
+                "totalCookies" : 0.0,
+                "cps" : 0.0,
+                "cursor_count" : 0,
+                "grandma_count" : 0,
+                "factory_count" : 0,
+            }
+    return {
+        "username": row["username"],
+        "cookies": row["cookies"],
+        "totalCookies": row["totalCookies"],
+        "cps": row["cps"],
+        "cursor_count": row["cursor_count"],
+        "grandma_count": row["grandma_count"],
+        "factory_count": row["factory_count"],
+    }
+
 
 @app.get("/rating", response_class=HTMLResponse)
 def rating_page():
